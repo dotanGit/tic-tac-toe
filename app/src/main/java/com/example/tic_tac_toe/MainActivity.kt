0e8a410c051334b1d97bbf49249c272e3c85812a
dotanGit <frenkeldotan1998@gmail.com>
2025-12-04 22:56:24 +0200

    user indicator and list of buttons

--- app/src/main/java/com/example/tic_tac_toe/MainActivity.kt
+++ app/src/main/java/com/example/tic_tac_toe/MainActivity.kt
@@ -1,20 +1,34 @@
 package com.example.tic_tac_toe
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var boardButtons: List<Button>
+    private var currentIsX = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        boardButtons = listOf(
+            findViewById(R.id.button_01), findViewById(R.id.button_02), findViewById(R.id.button_03),
+            findViewById(R.id.button_11), findViewById(R.id.button_12), findViewById(R.id.button_13),
+            findViewById(R.id.button_21), findViewById(R.id.button_22), findViewById(R.id.button_23),
+        )
+
+        boardButtons.forEach { btn ->
+            btn.setOnClickListener {
+                btn.text = if (currentIsX) "X" else "O"
+                btn.isEnabled = false
+                currentIsX = !currentIsX
+            }
         }
+
     }
 }
